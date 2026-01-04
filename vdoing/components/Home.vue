26cf2ce804b856379395136051fe4fb4f3136286
MrXiaoM <coolxiaom95@gmail.com>
2026-01-04 23:01:25 +0800

    fix wrong total posts length uses

--- vdoing/components/Home.vue
+++ vdoing/components/Home.vue
@@ -253,7 +253,7 @@ export default {
   },
   components: { NavLink, MainLayout, PostList, UpdateArticle, BloggerBar, CategoriesBar, TagsBar, Pagination },
   created() {
-    this.total = this.$sortPosts.length
+    this.total = this.$sortPostsHome.length
   },
   beforeMount() {
     this.isMQMobile = window.innerWidth < MOBILE_DESKTOP_BREAKPOINT ? true : false; // vupress在打包时不能在beforeCreate(),created()访问浏览器api（如window）
